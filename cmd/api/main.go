8cdcba68ac36fd2e78726e68559afa70af6e590f
Linux-DEX <sarabjeet.9353@gmail.com>
2026-06-27 16:38:45 +0530

    configuring postgresSQL connection on service startup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"todo_api/internal/config"
+	"todo_api/internal/database"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    var router *gin.Engine = gin.Default()
+	// loading application configuration for environment variable
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
 
-    // Do not trust proxy headers (e.g. X-Forwarded-For); use the direct client connection IP.
-    router.SetTrustedProxies(nil)
+	// Connect to PostgresSQL database using connection string
+	pool, err := database.Connect(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 
-    router.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Todo API is running!",
-            "status": "Success",
-        })
-    })
+	// closing database connection when application exits
+	defer pool.Close()
 
-    router.Run(":8080")
+	// creating gin router
+	router := gin.Default()
+
+	// Do not trust proxy headers (e.g. X-Forwarded-For); use the direct client connection IP.
+	router.SetTrustedProxies(nil)
+
+	router.GET("/", func(c *gin.Context) {
+		// map[string]interface{}
+		// map[string]any{}
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "Todo API is running!",
+			"status":   "Success",
+			"database": "Connected",
+		})
+	})
+
+	// start http server on port PORT
+	router.Run(":" + cfg.Port)
 }
